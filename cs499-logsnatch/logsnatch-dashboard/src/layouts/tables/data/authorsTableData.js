401c7eb3ab9cb53bf5032220ab2c04c06d57c377
conor <conorjdaut@gmail.com>
2026-02-22 20:19:40 -0600

    Convert submodule to normal folder

---
+++ cs499-logsnatch/logsnatch-dashboard/src/layouts/tables/data/authorsTableData.js
@@ -0,0 +1,62 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/function-component-definition */
+/**
+=========================================================
+* Material Dashboard 2 React - v2.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// Material Dashboard 2 React components
+import MDBox from "components/MDBox";
+import MDTypography from "components/MDTypography";
+import MDAvatar from "components/MDAvatar";
+import MDBadge from "components/MDBadge";
+
+// Images
+import team2 from "assets/images/team-2.jpg";
+import team3 from "assets/images/team-3.jpg";
+import team4 from "assets/images/team-4.jpg";
+
+export default function data() {
+  const Author = ({ image, name, email }) => (
+    <MDBox display="flex" alignItems="center" lineHeight={1}>
+      <MDAvatar src={image} name={name} size="sm" />
+      <MDBox ml={2} lineHeight={1}>
+        <MDTypography display="block" variant="button" fontWeight="medium">
+          {name}
+        </MDTypography>
+        <MDTypography variant="caption">{email}</MDTypography>
+      </MDBox>
+    </MDBox>
+  );
+
+  const Job = ({ title, description }) => (
+    <MDBox lineHeight={1} textAlign="left">
+      <MDTypography display="block" variant="caption" color="text" fontWeight="medium">
+        {title}
+      </MDTypography>
+      <MDTypography variant="caption">{description}</MDTypography>
+    </MDBox>
+  );
+
+  return {
+    columns: [
+      { Header: "Scan Start Time", accessor: "Scan Start Time", width: "45%", align: "left" },
+      { Header: "Scan Duration", accessor: "Scan Duration", align: "left" },
+      { Header: "Scan Results", accessor: "Scan Results", align: "center" },
+      { Header: "Warnings", accessor: "Warnings", align: "center" },
+      { Header: "Log Location", accessor: "Log Location", align: "center" },
+    ],
+
+    rows: [],
+  };
+}
